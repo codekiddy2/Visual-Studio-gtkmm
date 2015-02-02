e0bf11f1833c2845319b08bf8c374be95699388d
codekiddy2 <codekiddy@gmail.com>
2015-02-02 07:29:09 +0100

    replace pcre2 with pcre
    if you need new version let me know

--- projects/pcre2-10.00/release-32.bat
+++
@@ -1,9 +0,0 @@
-set ROOT_DIR=D:\gtkmm\output\Release\Win32
-set CL=@D:\gtkmm\properties\release-32.compiler
-set PATH=%ROOT_DIR%\bin;%PATH%
-set LIB=%ROOT_DIR%\lib;%LIB%
-set MOZBUILDDIR=D:\gtkmm\install\mozilla-build
-set MOZILLABUILD=%MOZBUILDDIR%
-set MOZ_TOOLS=%MOZBUILDDIR%moztools
-start /d "%USERPROFILE%" "" "%MOZILLABUILD%"\msys\bin\bash --login -i
-exit
