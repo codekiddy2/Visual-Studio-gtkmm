f9866166abc1c144eadb745dd44243f050ab42eb
codekiddy2 <codekiddy@gmail.com>
2015-02-04 00:14:53 +0100

    zlib test

--- projects/gettext-0.19.4/msvc-120/libintl.h
+++
@@ -1,8 +0,0 @@
-
-/* codekiddy -> create this header or copy paste the original */
-#if (!defined LIBINTL_H_INCLUDED) && (!defined _LIBINTL_H)
-#define LIBINTL_H_INCLUDED
-
-#include <libgnuintl.h>
-
-#endif /* LIBINTL_H_INCLUDED */
