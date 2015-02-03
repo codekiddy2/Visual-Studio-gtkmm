14d5fdba092e4b2b459ddc7a9b76c2ac36a37212
codekiddy2 <codekiddy@gmail.com>
2015-02-03 10:45:03 +0100

    Update latest version of gettest

--- projects/gettext-0.19.4/debug-64.bat
+++
@@ -1,8 +0,0 @@
-set ROOT_DIR=D:\gtkmm\output\Debug\x64
-set CL=@D:\gtkmm\build\gettext-0.19.4\debug-64.compiler
-set PATH=%ROOT_DIR%\bin;%PATH%
-set LIB=%ROOT_DIR%\lib;%LIB%
-set MOZBUILDDIR=D:\gtkmm\install\mozilla-build
-set MOZILLABUILD=%MOZBUILDDIR%
-set MOZ_TOOLS=%MOZBUILDDIR%moztools-x64
-start /d "%USERPROFILE%" "" "%MOZILLABUILD%"\msys\bin\bash --login -i
---
+++ projects/gettext-0.19.4/msvc-120/libintl.h
@@ -0,0 +1,8 @@
+
+/* codekiddy -> create this header or copy paste the original */
+#if (!defined LIBINTL_H_INCLUDED) && (!defined _LIBINTL_H)
+#define LIBINTL_H_INCLUDED
+
+#include <libgnuintl.h>
+
+#endif /* LIBINTL_H_INCLUDED */
