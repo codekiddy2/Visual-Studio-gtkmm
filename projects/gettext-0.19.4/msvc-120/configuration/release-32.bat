6e8751adeb46ff00d9ad6afff79931e706392c20
codekiddy2 <codekiddy@gmail.com>
2015-02-06 11:39:11 +0100

    update libintl compile settings

---
+++ projects/gettext-0.19.4/msvc-120/configuration/release-32.bat
@@ -0,0 +1,9 @@
+set ROOT_DIR=D:\GitHub\Visual-Studio-gtkmm\output\Release\Win32
+set CL=@D:\GitHub\Visual-Studio-gtkmm\projects\gettext-0.19.4\release-32.compiler
+set PATH=%ROOT_DIR%\bin;%PATH%
+set LIB=%ROOT_DIR%\lib;%LIB%
+set INCLUDE=%ROOT_DIR%\include;%ROOT_DIR%\include\iconv;%ROOT_DIR%\include\expat;%INCLUDE%
+set MOZBUILDDIR=D:\gtkmm\install\mozilla-build
+set MOZILLABUILD=%MOZBUILDDIR%
+set MOZ_TOOLS=%MOZBUILDDIR%moztools
+start /d "%USERPROFILE%" "" "%MOZILLABUILD%"\msys\bin\bash --login -i
