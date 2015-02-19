60e019ae89f56ddee545dceac0c05e7fef66e4b8
codekiddy2 <codekiddy@gmail.com>
2015-02-19 20:42:53 +0100

    update graph, add icon-theme setup

---
+++ projects/gnome-icon-theme-3.12.0/msvc-120/debug-64.bat
@@ -0,0 +1,3 @@
+set ROOT_DIR=D:\GitHub\Visual-Studio-gtkmm\output\Debug\x64
+set CL=@D:\GitHub\Visual-Studio-gtkmm\projects\gnome-icon-theme-3.12.0\debug-64.compiler
+start D:\gtkmm\install\msys64\mingw64_shell.bat
