8a5c550a726ad9a593dfa1313f6a24ac3769bd1c
codekiddy2 <codekiddy@gmail.com>
2015-02-19 23:52:30 +0100

    update ffi

--- projects/libffi-3.0.13/msvc-120/configuration/release-32.sh
+++ projects/libffi-3.0.13/msvc-120/configuration/release-32.sh
@@ -1,1 +1,1 @@
-./configure --enable-shared --enable-static CC=$(pwd)/msvcc.sh CXX=cl CPP='cl -nologo -EP' CXXCPP='cl -E' AR=lib LD=link CFLAGS='' CXXFLAGS='' CPPFLAGS='' LDFLAGS='' LIBS='' --build=i686-pc-mingw32 --prefix=/d/GitHub/Visual-Studio-gtkmm/output/Release/Win32
+./configure --enable-shared --enable-static CC=$(pwd)/msvcc.sh CXX=$(pwd)/msvcc.sh CPP='cl -nologo -EP' CXXCPP='cl -E' AR=lib LD=link CFLAGS='' CXXFLAGS='' CPPFLAGS='' LDFLAGS='' LIBS='' --build=i686-pc-mingw32 --host=i686-pc-mingw32 --prefix=/d/GitHub/Visual-Studio-gtkmm/output/Release/Win32
