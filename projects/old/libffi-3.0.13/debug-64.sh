7b93f8305e4ad7ba08c60761bddb629c7af29c96
codekiddy2 <codekiddy@gmail.com>
2015-02-21 10:13:23 +0100

    add missing files

--- projects/old/libffi-3.0.13/debug-64.sh
+++
@@ -1,1 +0,0 @@
-./configure --enable-shared --enable-static CC="$(pwd)/msvcc.sh -m64" CXX=cl CPP='cl -nologo -EP' CXXCPP='cl -E' AR=lib LD=link CFLAGS='' CXXFLAGS='' CPPFLAGS='' LDFLAGS='' LIBS='' --build=x86_64-w64-mingw32 --prefix=/d/GitHub/Visual-Studio-gtkmm/output/Debug/x64
