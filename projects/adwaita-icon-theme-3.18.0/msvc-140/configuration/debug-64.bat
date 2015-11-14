b5d4a50eece3429feb5fb1940aab4bb572c1bab5
codekiddy2 <codekiddy@gmail.com>
2015-11-14 06:16:50 +0100

    update dependency graph

--- projects/adwaita-icon-theme-3.18.0/msvc-140/configuration/debug-64.bat
+++ projects/adwaita-icon-theme-3.18.0/msvc-140/configuration/debug-64.bat
@@ -1,2 +1,2 @@
-set CL=@D:\GitHub\Visual-Studio-gtkmm\projects\gnome-icon-theme-3.18.0\debug-64.compiler
+set CL=@D:\Github\Visual-Studio-gtkmm\projects\adwaita-icon-theme-3.18.0\debug-64.compiler
 start D:\Github\Install\msys64\mingw64_shell.bat
