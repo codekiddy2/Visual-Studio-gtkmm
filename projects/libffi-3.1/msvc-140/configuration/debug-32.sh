647d40d71329b5abd88ac48a280d2bd3d81fec51
codekiddy2 <codekiddy@gmail.com>
2015-11-20 05:45:56 +0100

    release test 5

--- projects/libffi-3.1/msvc-140/configuration/debug-32.sh
+++ projects/libffi-3.1/msvc-140/configuration/debug-32.sh
@@ -1,1 +1,3 @@
-./configure --enable-shared --enable-static CC=$(pwd)/msvcc.sh CXX=$(pwd)/msvcc.sh CPP='cl -nologo -EP' CXXCPP='cl -E' AR=lib LD=link CFLAGS='' CXXFLAGS='' CPPFLAGS='' LDFLAGS='' LIBS='' --build=i686-pc-mingw32 --host=i686-pc-mingw32 --prefix=/d/GitHub/Visual-Studio-gtkmm/output/v120/Debug/Win32
+make clean
+./configure --enable-shared --enable-static CC=$(pwd)/msvcc.sh CXX=$(pwd)/msvcc.sh CPP='cl -nologo -EP' CXXCPP='cl -E' AR=lib LD=link CFLAGS='' CXXFLAGS='' CPPFLAGS='' LDFLAGS='' LIBS='' --build=i686-pc-mingw32 --host=i686-pc-mingw32 --prefix=/d/GitHub/Visual-Studio-gtkmm/output/v140/Debug/Win32
+make
--- projects/libffi-3.1/msvc-140/configuration/release-64.sh
+++ projects/libffi-3.1/msvc-140/configuration/release-64.sh
@@ -1,1 +1,3 @@
-./configure --enable-shared --enable-static CC="$(pwd)/msvcc.sh -m64" CXX="$(pwd)/msvcc.sh -m64" CPP='cl -nologo -EP' CXXCPP='cl -E' AR=lib LD=link CFLAGS='' CXXFLAGS='' CPPFLAGS='' LDFLAGS='' LIBS='' --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --prefix=/d/GitHub/Visual-Studio-gtkmm/output/v120/Release/x64
+make clean
+./configure --enable-shared --enable-static CC="$(pwd)/msvcc.sh -m64" CXX="$(pwd)/msvcc.sh -m64" CPP='cl -nologo -EP' CXXCPP='cl -E' AR=lib LD=link CFLAGS='' CXXFLAGS='' CPPFLAGS='' LDFLAGS='' LIBS='' --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --prefix=/d/GitHub/Visual-Studio-gtkmm/output/v140/Release/x64
+make
