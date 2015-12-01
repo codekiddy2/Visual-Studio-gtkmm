1d8f8e672279bd55bd188d514338227bed4aa69e
codekiddy2 <codekiddy@gmail.com>
2015-12-01 06:31:30 +0100

    x86 test stage 5

--- projects/libffi-3.1/msvc-140/configuration/debug-64.bat
+++ projects/libffi-3.1/msvc-140/configuration/debug-64.bat
@@ -2,3 +2,4 @@ set MOZBUILDDIR=D:\Github\Install\mozilla-build
 set MOZILLABUILD=%MOZBUILDDIR%
 set MOZ_TOOLS=%MOZBUILDDIR%moztools-x64
 start /d "%USERPROFILE%" "" "%MOZILLABUILD%"\msys\bin\bash --login -i
+exit
