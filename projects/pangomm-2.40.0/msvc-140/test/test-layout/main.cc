48d980d3e12a44a3336b21d5c53ed2f57dff5fc6
codekiddy <codekiddy@gmail.com>
2016-06-24 13:38:56 +0200

    upgrade pangomm

---
+++ projects/pangomm-2.40.0/msvc-140/test/test-layout/main.cc
@@ -0,0 +1,29 @@
+
+#include <cairomm/refptr.h>
+#include <cairomm/surface.h>
+#include <cairomm/context.h>
+#include <glibmm/refptr.h>
+#include <pangomm/layout.h>
+#include <gtkmm/window.h>
+
+class TestClass :
+	public Gtk::Window
+{
+public:
+	TestClass()
+	{
+		layout = create_pango_layout("blah");
+	}
+
+	Glib::RefPtr<Pango::Layout> layout;
+};
+
+int main(int argc, char* argv[])
+{
+	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "gtkmm.exe");
+
+	TestClass* instance = new TestClass;
+	app->run(*instance);
+	delete instance;
+	return 0;
+}
